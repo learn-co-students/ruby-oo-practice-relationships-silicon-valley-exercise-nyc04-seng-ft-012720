09bea37500901685e4dfc895d0b8e4189121869f
Tororoi <cantwell.tom@gmail.com>
2020-02-04 08:52:57 -0500

    Done.

--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,42 @@
 class VentureCapitalist
 
+    attr_reader :name
+    attr_accessor :total_worth
+
+    @@all = []
+
+    def initialize(name, total_worth)
+        @name = name
+        @total_worth = total_worth
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.tres_commas_club
+        self.all.select {|vc| vc.total_worth > 1_000_000_000}
+    end
+
+    def offer_contract(startup, type, investment)
+        FundingRound.new(startup, self, type, investment)
+    end
+
+    def funding_rounds
+        FundingRound.all.select {|fr| fr.venture_capitalist == self}
+    end
+
+    def portfolio
+        funding_rounds.map {|fr| fr.startup}.uniq
+    end
+
+    def biggest_investment
+        funding_rounds.max_by {|fr| fr.investment}
+    end
+
+    def invested(domain_str)
+        relevant_fr = funding_rounds.select {|fr| fr.startup.domain == domain_str}
+        relevant_fr.reduce(0) {|sum, fr| sum + fr.investment}
+    end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -3,6 +3,24 @@ require_relative '../config/environment.rb'
 def reload
   load 'config/environment.rb'
 end
+
+chess = Startup.new("chess", "Tom", "url a")
+checkers = Startup.new("checkers", "Mavi", "url b")
+mancala = Startup.new("mancala", "Tom", "url c")
+frogger = Startup.new("frogger", "Daryl", "url d")
+
+joe = VentureCapitalist.new("Joe", 1_000_000_001)
+gurjot = VentureCapitalist.new("Gurjot", 2_000_000)
+abid = VentureCapitalist.new("Abid", 1_500_000_000)
+
+chess.sign_contract(joe, "pre-seed", 500_000)
+chess.sign_contract(joe, "pre-seed", 400_000)
+chess.sign_contract(gurjot, "pre-seed", 400_000)
+checkers.sign_contract(joe, "pre-seed", 200_000)
+chess.sign_contract(abid, "pre-seed", 600_000)
+mancala.sign_contract(joe, "pre-seed", 500_000)
+mancala.sign_contract(gurjot, "pre-seed", 300_000)
+frogger.sign_contract(joe, "pre-seed", 1_000_000)
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
